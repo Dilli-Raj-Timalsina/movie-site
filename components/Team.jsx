0d9fca14d476950c6f94433e86a713d5809f93a8
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2024-05-25 10:26:48 +0545

    chore: meet our section fixed

--- components/Team.jsx
+++ components/Team.jsx
@@ -31,7 +31,7 @@ export default function Team() {
                     ></ImageBox>
                 </div>
             </div> */}
-            <div className="grid grid-cols-4  h-fit  md:gap-x-10 gap-y-4 md:px-24 md:py-20  px-2 py-10 bg-gray-800  md:gap-x-5  ">
+            <div className="grid grid-cols-4  h-fit  md:gap-x-10 gap-y-4 md:px-24 md:py-15  px-2 py-10 bg-gray-800  md:gap-x-5  ">
                 <ImageBox
                     imgurl={"/director.png"}
                     name="Prem Lama"
@@ -60,7 +60,7 @@ export default function Team() {
 function ImageBox({ imgurl, name, position }) {
     return (
         <div className="text-start ">
-            <img src={`${imgurl}`} alt="" className="md:w-72 md:h-96 w-56 h-72" />
+            <img src={`${imgurl}`} alt="" className="md:w-64 md:h-80 w-56 h-72" />
             <h1 className="text-white text-xs md:text-4xl font-bold mt-6 text-nowrap	">
                 {name}
             </h1>
